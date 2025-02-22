ce8f435b3bee0919fc7f9a23a9210ed65c3ed315
DESKTOP-JJ86S6U\BreezyWeezy <eddinetaki087@gmail.com>
2025-02-22 18:30:00 +0100

    new changes

---
+++ app/src/main/java/com/le/app/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.le.app
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.cars.reels.Reels
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        Reels.enableInterAd(this,true,1)
+        Reels.enableBannerAd(this,true, frequency = 1)
+        Reels.enableVideoAd(this,true, frequency = 1)
+
+    }
+}
